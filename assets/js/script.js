d8b7ff430a2d4b3d793b43d9365187a659d42ba5
Hailey Flora <Haileycflora@gmail.com>
2023-03-01 21:50:54 -0500

    update getGeoWeather()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,41 @@
 var weatherAPIKey = "92ac65368ccc7477640749d69fd94759";
 var searchBar = $("#search-bar");
 var searchBtn = $("#search-button");
-var lat
-var long
+
+var lat;
+var long;
+var id;
+var temp;
+var windSpeed;
 
 $(searchBtn).on("click", function() {
     var city = searchBar.val()
 
     // we'll call all three functions
-    getGeolocation(city)
-    getWeather()
+    getGeolocation(city);
+    // getWeather()
     console.log(searchBtn.val());
 })
 
 // a function that gets the geolocation
-function getGeolocation(cityName) {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${weatherAPIKey}`)
-        .then(data => {
-            lat = data[0].lat
-            long = data[0].lon
+function getGeolocation(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPIKey}&units=imperial`) //imperial is converting to fahrenheit 
+    .then(response => response.json())
+        .then(data => {data.main.temp
+console.log(data);
+console.log(data.main.temp);
         })
 }
 
 // a function that gets the weather info based of the geo
 function getGeoWeather() {
-    fetch(``)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`)
+        .then(data => {
+            id = data[1].id
+            temp = data[2].temp
+            windSpeed = data[4].speed
+
+        })
 }
 
 // a function that does a loop and creates elements to show each day
